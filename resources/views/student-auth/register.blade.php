cf3d0d094a409b5a3f97fcd417c9ede075905cbd
Czercie Lhyanne Basco <160035392+czrclhynn@users.noreply.github.com>
2026-06-23 12:42:07 +0800

    Add placeholder to email input in registration form

--- resources/views/student-auth/register.blade.php
+++ resources/views/student-auth/register.blade.php
@@ -25,7 +25,7 @@
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
+                    <input type="email" name="email" class="form-control" placeholder="example@gmail.com" value="{{ old('email') }}" required>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Password</label>
